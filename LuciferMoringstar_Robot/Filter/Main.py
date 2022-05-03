32c6af436a6aef389a2287392a84d46ae6602fc8
DaDa <99330762+DadaOffl@users.noreply.github.com>
2022-05-03 08:02:26 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -305,7 +305,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton(' 𝙅𝙊𝙄𝙉 𝙈𝙊𝙑𝙄𝙀 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 ⭐ ', url='https://t.me/minitheaterlinkloop'),
+                        InlineKeyboardButton(' 𝙅𝙊𝙄𝙉 𝙈𝙊𝙑𝙄𝙀 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 ⭐ ', url='https://t.me/Dada_Linkz'),
                     ]
                     ]
                 
@@ -336,7 +336,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton(' 𝙅𝙊𝙄𝙉 𝙈𝙊𝙑𝙄𝙀 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 ⭐ ', url='https://t.me/minitheaterlinkloop'),
+                        InlineKeyboardButton(' 𝙅𝙊𝙄𝙉 𝙈𝙊𝙑𝙄𝙀 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 ⭐ ', url='https://t.me/Dada_Linkz'),
                     ]
                     ]
                 
